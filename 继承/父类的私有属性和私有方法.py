f2ffe8e22266d2a6515270071f543863b88cf259
骆明 <736704198@qq.com>
2020-08-26 08:32:13 +0800

    父类私有属性和私有方法

---
+++ 继承/父类的私有属性和私有方法.py
@@ -0,0 +1,35 @@
+# !/usr/bin/python3
+# -*- coding:utf-8 -*- 
+# author: Ming Luo
+# time: 2020/8/25 14:32
+
+# 1. 子类对象不能再自己的方法内部，直接访问父类的私有属性或私有方法
+# 2. 子类对象可以通过父类的公有方法间接访问到私有属性或私有方法
+# 私有属性、方法 是对象的隐私，不对外公开，外界以及子类都不能直接访问
+# 私有属性、方法 通常用于做一些内部的事情
+
+
+class A:
+    def __init__(self):
+        self.num1 = 1
+        self.__num2 = 2
+
+    def test1(self):
+        print("test 1 : %d %d" % (self.num1, self.__num2))
+
+    def __test2(self):
+        print("私有方法 test 2: %d %d" % (self.num1, self.__num2))
+
+
+class B(A):
+    def demo(self):
+        print("class B")
+
+
+b = B()
+# 在外界不能直接访问私有属性和私有方法
+b.__num2
+b.__test2()
+
+b._A__test2()
+print(b._A__num2)
