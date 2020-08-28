64111849689e757396e8b21da82668dd15f03423
骆明 <736704198@qq.com>
2020-08-28 13:24:01 +0800

    包

---
+++ package/build/lib/lm/receive_message.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python3
+# -*- coding:utf-8 -*- 
+# author: Ming Luo
+# time: 2020/8/28 11:33
+
+
+def receive():
+    print("receive 函数")
+
+
+
+
---
+++ package/build/lib/lm/send_message.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python3
+# -*- coding:utf-8 -*- 
+# author: Ming Luo
+# time: 2020/8/28 11:33
+
+
+def send():
+    print("send 函数")
+
+
+
